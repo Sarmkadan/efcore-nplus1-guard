9b5452fb216a85a359b6e72ad0e2ca23b2bb9b9b
Your Name <you@example.com>
2022-04-08 19:23:50 +0300

    Argument validation, NPlusOneGuardOptions

--- src/NPlusOneGuardOptions.cs
+++ src/NPlusOneGuardOptions.cs
@@ -15,3 +15,35 @@ namespace EfCoreNPlusOneGuard
         public List<string> IgnoredQueryPatterns { get; set; } = new List<string>();
     }
 }
+
+/// <summary>
+/// Options for the N+1 Guard.
+/// </summary>
+public class NPlusOneGuardOptions
+{
+    /// <summary>
+    /// The threshold for detecting N+1 queries.
+    /// </summary>
+    /// <value>The threshold.</value>
+    public int Threshold { get; set; } = 5;
+    /// <summary>
+    /// The time window for detecting N+1 queries.
+    /// </summary>
+    /// <value>The detection window.</value>
+    public TimeSpan DetectionWindow { get; set; } = TimeSpan.FromSeconds(2);
+    /// <summary>
+    /// Whether to throw an exception when an N+1 query is detected.
+    /// </summary>
+    /// <value><c>true</c> if throw on detection; otherwise, <c>false</c>.</value>
+    public bool ThrowOnDetection { get; set; } = false;
+    /// <summary>
+    /// Whether to log an N+1 query detection.
+    /// </summary>
+    /// <value><c>true</c> if log on detection; otherwise, <c>false</c>.</value>
+    public bool LogOnDetection { get; set; } = true;
+    /// <summary>
+    /// Patterns of queries to ignore.
+    /// </summary>
+    /// <value>The ignored query patterns.</value>
+    public List<string> IgnoredQueryPatterns { get; set; } = new List<string>();
+}
