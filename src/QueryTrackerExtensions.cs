11626f33c803556fbf25992e4b8f6d4247264e9f
Vladyslav Zaiets <rutova2@gmail.com>
2022-04-27 10:10:45 +0300

    a couple of QueryTracker extensions

---
+++ src/QueryTrackerExtensions.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+namespace EfCoreNPlusOneGuard;
+
+/// <summary>
+/// Provides extension methods for <see cref="QueryTracker"/>.
+/// </summary>
+public static class QueryTrackerExtensions
+{
+    /// <summary>
+    /// Tracks multiple query executions sequentially.
+    /// </summary>
+    /// <param name="tracker">The <see cref="QueryTracker"/> instance.</param>
+    /// <param name="sqlCommands">The collection of SQL commands to track.</param>
+    /// <exception cref="ArgumentNullException">Thrown when tracker or sqlCommands is null.</exception>
+    public static void TrackBatch(this QueryTracker tracker, IEnumerable<string> sqlCommands)
+    {
+        ArgumentNullException.ThrowIfNull(tracker);
+        ArgumentNullException.ThrowIfNull(sqlCommands);
+
+        foreach (var sql in sqlCommands)
+        {
+            tracker.TrackExecution(sql);
+        }
+    }
+
+    /// <summary>
+    /// Resets the tracker and returns the tracker instance for fluent usage.
+    /// </summary>
+    /// <param name="tracker">The <see cref="QueryTracker"/> instance.</param>
+    /// <returns>The original tracker instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when tracker is null.</exception>
+    public static QueryTracker ResetAndReturn(this QueryTracker tracker)
+    {
+        ArgumentNullException.ThrowIfNull(tracker);
+        tracker.Reset();
+        return tracker;
+    }
+
+    /// <summary>
+    /// Tracks a query execution if the command text is not null or empty.
+    /// </summary>
+    /// <param name="tracker">The <see cref="QueryTracker"/> instance.</param>
+    /// <param name="commandText">The SQL command text.</param>
+    /// <exception cref="ArgumentNullException">Thrown when tracker is null.</exception>
+    public static void TrackExecutionSafe(this QueryTracker tracker, string? commandText)
+    {
+        ArgumentNullException.ThrowIfNull(tracker);
+        if (!string.IsNullOrEmpty(commandText))
+        {
+            tracker.TrackExecution(commandText);
+        }
+    }
+}
