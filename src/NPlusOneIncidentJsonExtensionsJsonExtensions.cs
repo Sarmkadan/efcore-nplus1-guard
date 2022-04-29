6bc75ecdffc66d82b55066874ffcff775ca2974d
Vladyslav Zaiets <rutova2@gmail.com>
2022-04-29 08:33:15 +0300

    Serialize NPlusOneIncidentJsonExtensionsJson properly

---
+++ src/NPlusOneIncidentJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,75 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EfCoreNPlusOneGuard;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="NPlusOneIncident"/>.
+/// </summary>
+public static class NPlusOneIncidentJsonExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="NPlusOneIncident"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The incident to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the incident.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this NPlusOneIncident value, bool indented = false) =>
+        value is null
+            ? throw new ArgumentNullException(nameof(value))
+            : JsonSerializer.Serialize(value, indented
+                ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+                : _jsonOptions);
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="NPlusOneIncident"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized incident, or null if the JSON is empty, whitespace, or invalid.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static NPlusOneIncident? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return string.IsNullOrWhiteSpace(json)
+            ? null
+            : JsonSerializer.Deserialize<NPlusOneIncident>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="NPlusOneIncident"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized incident if successful, otherwise null.</param>
+    /// <returns>True if deserialization succeeded; false otherwise (including JsonException).</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out NPlusOneIncident? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = default;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<NPlusOneIncident>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
