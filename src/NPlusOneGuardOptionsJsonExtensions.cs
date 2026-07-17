339f3eee6fdd7fffbf02db35a29cbcad9d5f5364
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 19:07:11 +0300

    small refactor - NPlusOneGuardOptionsJsonExtensions

--- src/NPlusOneGuardOptionsJsonExtensions.cs
+++ src/NPlusOneGuardOptionsJsonExtensions.cs
@@ -22,23 +22,28 @@ namespace EfCoreNPlusOneGuard
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
         /// <returns>A JSON string representation of the options.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
-        public static string ToJson(this NPlusOneGuardOptions value, bool indented = false) =>
-            value is null
-                ? throw new ArgumentNullException(nameof(value))
-                : JsonSerializer.Serialize(value, indented
+        public static string ToJson(this NPlusOneGuardOptions value, bool indented = false)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            return JsonSerializer.Serialize(
+                value,
+                indented
                     ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-                    : _jsonOptions);
+                    : _jsonOptions
+            );
+        }
 
         /// <summary>
         /// Deserializes a JSON string into a <see cref="NPlusOneGuardOptions"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
         /// <returns>The deserialized options, or null if the JSON is empty or whitespace.</returns>
-        /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
         /// <exception cref="JsonException">Thrown when JSON parsing fails.</exception>
         public static NPlusOneGuardOptions? FromJson(string json)
         {
-            ArgumentException.ThrowIfNullOrEmpty(json);
+            ArgumentNullException.ThrowIfNull(json);
 
             return string.IsNullOrWhiteSpace(json)
                 ? null
@@ -51,10 +56,10 @@ namespace EfCoreNPlusOneGuard
         /// <param name="json">The JSON string to deserialize.</param>
         /// <param name="value">Receives the deserialized options if successful, otherwise null.</param>
         /// <returns>True if deserialization succeeded; false otherwise (including JsonException).</returns>
-        /// <exception cref="ArgumentException"><paramref name="json"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
         public static bool TryFromJson(string json, out NPlusOneGuardOptions? value)
         {
-            ArgumentException.ThrowIfNullOrEmpty(json);
+            ArgumentNullException.ThrowIfNull(json);
 
             value = default;
 
